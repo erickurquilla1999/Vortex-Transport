26ff6615397455959272e2679d4e3b779c9b9a64
erickurquilla1999 <erickurquillla1999@gmail.com>
2024-04-11 23:13:03 -0400

    add gauss quadrature as property of Evolve_element class

--- Evolve.cpp
+++ Evolve.cpp
@@ -8,14 +8,15 @@
 
 Evolve_element::Evolve_element() {}
 
-Evolve_element::Evolve_element(Element* this_elem, Element* right_elem, Element* left_elem, Element* vertical_elem):
+Evolve_element::Evolve_element(Element* this_elem, Element* right_elem, Element* left_elem, Element* vertical_elem, const std::vector<std::vector<double>>& gau_int_l):
     
     // Initialize Evolve_element properties    
     this_element(this_elem),            // this is the element to be evolved in time
     right_element(right_elem),          // this the element to the right to the element to be evolved in time
     left_element(left_elem),            // this the element to the left to the element to be evolved in time
-    vertival_element(vertical_elem)     // this the element in the vertical direction to the element to be evolved in time
- 
+    vertival_element(vertical_elem),    // this the element in the vertical direction to the element to be evolved in time
+    gau_integ_line(gau_int_l)           // 
+
     {
 
     std::cout << "Element : " << this->this_element->hidrodynamics_vector_u[0][0] << std::endl;
