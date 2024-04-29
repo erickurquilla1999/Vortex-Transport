95787e23bcf9d8c0a8131153b3f3c27166c7135f
erickurquilla1999 <erickurquillla1999@gmail.com>
2024-04-28 22:01:19 -0400

    add new input parameter for the initialization type of hidrodynamics state U

--- Parameters.cpp
+++ Parameters.cpp
@@ -52,6 +52,7 @@ parameters read_input_files(int argc, char* argv[]) {
     params.number_time_steps = std::stoi(paramMap["number_time_steps"]);
     params.time_step = std::stod(paramMap["simulation_time"]) / std::stod(paramMap["number_time_steps"]);
     params.write_every_steps = std::stoi(paramMap["write_every_steps"]);
-
+    params.U_initialization_type = std::stoi(paramMap["U_initialization_type"]);
+   
     return params;
 }
