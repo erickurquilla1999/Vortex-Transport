882fee5caa13c4b77ba79202f6a427e4f979f2f9
erickurquilla1999 <erickurquillla1999@gmail.com>
2024-04-30 18:46:01 -0400

    add script to compute l2 state error

---
+++ plottools/l2stateerror.py
@@ -0,0 +1,123 @@
+import pandas as pd
+import glob
+import matplotlib.pyplot as plt
+import numpy as np
+import h5py
+from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,AutoMinorLocator,LogLocator)
+import matplotlib as mpl
+import matplotlib.ticker
+from matplotlib import cm
+import matplotlib.tri as tri
+from matplotlib.animation import FuncAnimation
+import matplotlib.tri as tri
+
+from plotutilities import lagrange_basis_reference_space, gauss_quadrature, w, read_input_file
+
+mpl.rcParams['font.size'] = 22
+mpl.rcParams['font.family'] = 'serif'
+mpl.rc('text', usetex=True)
+mpl.rcParams['xtick.major.size'] = 7
+mpl.rcParams['xtick.major.width'] = 2
+mpl.rcParams['xtick.major.pad'] = 8
+mpl.rcParams['xtick.minor.size'] = 4
+mpl.rcParams['xtick.minor.width'] = 2
+mpl.rcParams['ytick.major.size'] = 7
+mpl.rcParams['ytick.major.width'] = 2
+mpl.rcParams['ytick.minor.size'] = 4
+mpl.rcParams['ytick.minor.width'] = 2
+mpl.rcParams['axes.linewidth'] = 2
+
+################################################################################
+# L2 STATE ERROR
+################################################################################
+
+# Get directory names
+dir_names = glob.glob("output/step_*")
+dir_names = sorted(dir_names, key=lambda x: int(x.lstrip("output/step_")))
+
+# t=0.0 : First data file generated
+file_names_i = glob.glob(dir_names[0] + "/element_*.txt")
+file_names_i = sorted(file_names_i, key=lambda x: int(x.split("/element_")[1].rstrip(".txt")))
+
+# t=14.1421 : Last data file generated
+file_names_f = glob.glob(dir_names[-1] + "/element_*.txt")
+file_names_f = sorted(file_names_f, key=lambda x: int(x.split("/element_")[1].rstrip(".txt")))
+
+# Get grid file names matching the pattern "element*.txt"
+grid_file_names = glob.glob("grid/element_*.txt")
+grid_file_names = sorted(grid_file_names, key=lambda x: int(x.lstrip("grid/element_").rstrip(".txt")))
+
+# save integral over space of hidrodynamic vector u
+integral = np.zeros(4)
+
+# runs over elements
+for i in range(len(file_names_i)):
+
+    # read grid data
+    data = read_input_file(grid_file_names[i])
+    # save elements vertex
+    vertices_coords_phys_space =  [ [ data[1] , data[2] ] , [ data[3] , data[4] ] , [ data[5] , data[6] ] ]
+    # compute jacobian
+    jacobian = [
+        [vertices_coords_phys_space[1][0] - vertices_coords_phys_space[0][0], vertices_coords_phys_space[2][0] - vertices_coords_phys_space[0][0]],
+        [vertices_coords_phys_space[1][1] - vertices_coords_phys_space[0][1], vertices_coords_phys_space[2][1] - vertices_coords_phys_space[0][1]]
+    ]
+    # Compute determinant of jacobian
+    determinant_jacobian = jacobian[0][0] * jacobian[1][1] - jacobian[1][0] * jacobian[0][1]
+
+    # read data at t=0.0
+    df_i = pd.read_csv(file_names_i[i], sep='\s+')
+
+    x_ele_i = np.array(df_i['x'])
+    y_ele_i = np.array(df_i['y'])
+    u0_ele_i = np.array(df_i['u0'])
+    u1_ele_i = np.array(df_i['u1'])
+    u2_ele_i = np.array(df_i['u2'])
+    u3_ele_i = np.array(df_i['u3'])
+
+    # read data at t=14.1421
+    df_f = pd.read_csv(file_names_f[i], sep='\s+')
+
+    x_ele_f = np.array(df_f['x'])
+    y_ele_f = np.array(df_f['y'])
+    u0_ele_f = np.array(df_f['u0'])
+    u1_ele_f = np.array(df_f['u1'])
+    u2_ele_f = np.array(df_f['u2'])
+    u3_ele_f = np.array(df_f['u3'])
+
+    # ( delta u ) ^ 2
+    del_u0 = (u0_ele_f - u0_ele_i)**2
+    del_u1 = (u1_ele_f - u1_ele_i)**2
+    del_u2 = (u2_ele_f - u2_ele_i)**2
+    del_u3 = (u3_ele_f - u3_ele_i)**2
+
+    p = 3
+
+    basis_in_quad_points = np.zeros( ( int( ( p + 1 ) * ( p + 2) / 2 ) , len( w ) ) )
+
+    # computing basis function in the gauss quadrature points
+    for i in range(len(w)):
+        basis = lagrange_basis_reference_space( p , gauss_quadrature[i] )
+        for j in range( int( ( p + 1 ) * ( p + 2) / 2 ) ):
+            basis_in_quad_points[j][i] = basis[j]
+
+    delta_U_in_qp = np.zeros( ( len(w) , 4 ) )
+
+    # intepolating delta u to quadrature points
+    for i in range(len(w)):
+        for j in range( int( ( p + 1 ) * ( p + 2) / 2 ) ):
+            delta_U_in_qp[i][0] += del_u0[j] * basis_in_quad_points[j][i]
+            delta_U_in_qp[i][1] += del_u1[j] * basis_in_quad_points[j][i]
+            delta_U_in_qp[i][2] += del_u2[j] * basis_in_quad_points[j][i]
+            delta_U_in_qp[i][3] += del_u3[j] * basis_in_quad_points[j][i]
+
+    # integrating 
+    integral[0] += determinant_jacobian * np.sum( np.array(w) * delta_U_in_qp[:, 0])
+    integral[1] += determinant_jacobian * np.sum( np.array(w) * delta_U_in_qp[:, 1])
+    integral[2] += determinant_jacobian * np.sum( np.array(w) * delta_U_in_qp[:, 2])
+    integral[3] += determinant_jacobian * np.sum( np.array(w) * delta_U_in_qp[:, 3])
+
+# stimating error
+error = np.sqrt( ( 1.0 / 25.0 ) * integral )
+print(f' L2 state error = {error}')
+
---
+++ plottools/plotutilities.py
@@ -0,0 +1,107 @@
+import pandas as pd
+import glob
+import matplotlib.pyplot as plt
+import numpy as np
+import h5py
+from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,AutoMinorLocator,LogLocator)
+import matplotlib as mpl
+import matplotlib.ticker
+from matplotlib import cm
+import matplotlib.tri as tri
+from matplotlib.animation import FuncAnimation
+import matplotlib.tri as tri
+
+################################################################################
+# Lagrange basis functions
+################################################################################
+
+def lagrange_basis_reference_space(p, coords_ref_spa):
+    # Coordinates in reference space (xi, eta)
+    xi, eta = coords_ref_spa[0], coords_ref_spa[1]
+    
+    # phi(xi, eta) for each interior node in the element
+    phi = [0.0] * ((p + 1) * (p + 2) // 2)
+
+    if p == 0:
+        phi[0] = 1.0
+    elif p == 1:
+        phi[0] = 1.0 - xi - eta
+        phi[1] = xi
+        phi[2] = eta
+    elif p == 2:
+        phi[0] = 1.0 - 3.0 * xi - 3.0 * eta + 2.0 * xi * xi + 4.0 * xi * eta + 2.0 * eta * eta
+        phi[1] = 4.0 * xi - 4.0 * xi * xi - 4.0 * xi * eta
+        phi[2] = -xi + 2.0 * xi * xi
+        phi[3] = 4.0 * eta - 4.0 * xi * eta - 4.0 * eta * eta
+        phi[4] = 4.0 * xi * eta
+        phi[5] = -eta + 2.0 * eta * eta
+    elif p == 3:
+        phi[0] = 1.0 - 11.0/2.0*xi - 11.0/2.0*eta + 9.0*xi*xi + 18.0*xi*eta + 9.0*eta*eta - 9.0/2.0*xi*xi*xi - 27.0/2.0*xi*xi*eta - 27.0/2.0*xi*eta*eta - 9.0/2.0*eta*eta*eta
+        phi[1] = 9.0*xi - 45.0/2.0*xi*xi - 45.0/2.0*xi*eta + 27.0/2.0*xi*xi*xi + 27.0*xi*xi*eta + 27.0/2.0*xi*eta*eta
+        phi[2] = -9.0/2.0*xi + 18.0*xi*xi + 9.0/2.0*xi*eta - 27.0/2.0*xi*xi*xi -27.0/2.0*xi*xi*eta
+        phi[3] = xi - 9.0/2.0*xi*xi + 9.0/2.0*xi*xi*xi
+        phi[4] = 9.0*eta - 45.0/2.0*xi*eta - 45.0/2.0*eta*eta + 27.0/2.0*xi*xi*eta + 27.0*xi*eta*eta + 27.0/2.0*eta*eta*eta
+        phi[5] = 27.0*xi*eta - 27.0*xi*xi*eta - 27.0*xi*eta*eta
+        phi[6] = -9.0/2.0*xi*eta + 27.0/2.0*xi*xi*eta
+        phi[7] = -9.0/2.0*eta + 9.0/2.0*xi*eta + 18.0*eta*eta - 27.0/2.0*xi*eta*eta - 27.0/2.0*eta*eta*eta
+        phi[8] = -9.0/2.0*xi*eta + 27.0/2.0*xi*eta*eta
+        phi[9] = eta - 9.0/2.0*eta*eta + 9.0/2.0*eta*eta*eta
+    else:
+        print("ERROR: Unsupported p order for lagrange_basis_reference_space")
+        exit(EXIT_FAILURE)
+
+    # Return the value of all Lagrange basis function at position (xi, eta) in reference space
+    # First index represents the node number, running between 0 and (p + 1) * (p + 2) / 2
+    # The value contained in the Lagrange basis function evaluated at point (xi, eta) in reference space
+    return phi
+
+################################################################################
+# Gauss quadrature integration
+################################################################################
+
+x = [
+    0.333333333333333, 0.333333333333333, 0.028844733232685, 0.485577633383657,
+    0.485577633383657, 0.485577633383657, 0.485577633383657, 0.028844733232685,
+    0.781036849029926, 0.109481575485037, 0.109481575485037, 0.109481575485037,
+    0.109481575485037, 0.781036849029926, 0.141707219414880, 0.307939838764121,
+    0.307939838764121, 0.550352941820999, 0.550352941820999, 0.141707219414880,
+    0.307939838764121, 0.141707219414880, 0.550352941820999, 0.307939838764121,
+    0.141707219414880, 0.550352941820999, 0.025003534762686, 0.246672560639903,
+    0.246672560639903, 0.728323904597411, 0.728323904597411, 0.025003534762686,
+    0.246672560639903, 0.025003534762686, 0.728323904597411, 0.246672560639903,
+    0.025003534762686, 0.728323904597411, 0.009540815400299, 0.066803251012200,
+    0.066803251012200, 0.923655933587500, 0.923655933587500, 0.009540815400299,
+    0.066803251012200, 0.009540815400299, 0.923655933587500, 0.066803251012200,
+    0.009540815400299, 0.923655933587500
+]
+
+w = [
+    0.045408995191377, 0.018362978878233, 0.018362978878233, 0.018362978878233,
+    0.022660529717764, 0.022660529717764, 0.022660529717764, 0.036378958422710,
+    0.036378958422710, 0.036378958422710, 0.036378958422710, 0.036378958422710,
+    0.036378958422710, 0.014163621265528, 0.014163621265528, 0.014163621265528,
+    0.014163621265528, 0.014163621265528, 0.014163621265528, 0.004710833481867,
+    0.004710833481867, 0.004710833481867, 0.004710833481867, 0.004710833481867,
+    0.004710833481867
+]
+
+gauss_quadrature = np.zeros( ( len(w) , 2 ) )
+
+for i in range(len(w)):
+    gauss_quadrature[i] = [x[2 * i], x[2 * i + 1]]
+
+################################################################################
+# READ GRID FILE
+################################################################################
+
+def read_input_file(file_path):
+    data = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            if line.strip():  # Skip empty lines
+                key, value = line.strip().split('=')
+                if key.startswith('coordinate'):
+                    data.append(float(value))
+                elif key in ['element_number', 'type', 'right_element', 'left_element', 'vertical_element']:
+                    data.append(int(value))
+    return data
